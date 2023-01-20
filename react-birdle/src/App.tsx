391367c86137f27560e560edfec53233bd03618c
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-20 16:34:09 -0500

    app file functions rendering properly and passing props to Game component

--- react-birdle/src/App.tsx
+++ react-birdle/src/App.tsx
@@ -1,36 +1,52 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios'
+import  Game  from './components/Game';
 import './App.css';
 
+
+
 const App = () => {
+// These are the variables where data gathered from database are stored and passed as State
+const [birdArray, setBirdArray] = useState([{
+  id: 0,
+  name: "",
+  image: "",
+}])
+const [birdWord, setBirdWord] = useState("")
 
-const url = 'http://localhost:8000/api/birdle'
+//This variable makes toggling the url for testing easier
+const url = 'http://localhost:8000/api/birdle/'
 
-const [birdArray, setBirdArray] = useState([])
-const [birdWord, setBirdWord] = useState('')
-
-
-const getWord: any = () => {
+// This checks to see if birdArray is already loaded. If not, it does an axios get request to the server for the array of data. Else, it returns the already loaded array.
+const getWord= (): any => {
+  let i = Math.floor(Math.random() * birdArray.length)
+  if (birdArray[i].name.length < 8) {
   axios.get(url)
   .then((response) => setBirdArray(response.data), (err) => console.log(err))
   .catch((error) => console.log(error))
+  } else {return birdArray}
 }
 
-const pickBird: any = () => {
-  let pick = Math.floor(Math.random() * birdArray.length)
-  setBirdWord(birdArray[pick])
-}
-
-useEffect (() => {
+//This function sets the birdWord state in order to pass it to the Game child component.
+const pickBird = (): any => {
   getWord()
-  pickBird()
-}, [])
+  let pick = Math.floor(Math.random() * birdArray.length)
+  setBirdWord(birdArray[pick].name)
+}
+
+//I was having issue getting the word to load on page refresh--even if it was already determined. This if statement solves takes care of this, and a new word is selected each time. This may not be ideal in the long run, but works for now.
+useEffect (() => {
+  if (birdWord.length < 8) {
+    pickBird()}
+}, )
+
 
 console.log(birdArray)
 console.log(birdWord)
   return (
     <div className="App">
       
+      <Game bird={birdWord} />
     </div>
   );
 }
---
+++ react-birdle/src/components/Game.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// interface Props {
+// id: number,
+// name: string,
+// image: string
+// }
+
+const Game = ({bird}: {bird: string}) => {
+
+
+
+    return (
+        <>
+            <p>{bird}</p>
+
+        </>
+  );
+}
+
+export default Game
